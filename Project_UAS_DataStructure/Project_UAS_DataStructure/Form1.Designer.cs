aa7e0792a5f29d1cae8f21b334d774744ad1b32d
ArthurPts <141316872+ArthurPts@users.noreply.github.com>
2024-12-08 22:40:32 +0700

    start

---
+++ Project_UAS_DataStructure/Project_UAS_DataStructure/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Project_UAS_DataStructure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxRaw = new System.Windows.Forms.ListBox();
+            this.listBoxHasil = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ProcessTime = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBoxRaw
+            // 
+            this.listBoxRaw.FormattingEnabled = true;
+            this.listBoxRaw.ItemHeight = 16;
+            this.listBoxRaw.Location = new System.Drawing.Point(28, 101);
+            this.listBoxRaw.Name = "listBoxRaw";
+            this.listBoxRaw.Size = new System.Drawing.Size(333, 676);
+            this.listBoxRaw.TabIndex = 2;
+            // 
+            // listBoxHasil
+            // 
+            this.listBoxHasil.FormattingEnabled = true;
+            this.listBoxHasil.ItemHeight = 16;
+            this.listBoxHasil.Location = new System.Drawing.Point(605, 101);
+            this.listBoxHasil.Name = "listBoxHasil";
+            this.listBoxHasil.Size = new System.Drawing.Size(345, 676);
+            this.listBoxHasil.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Masukan jumlah data :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(600, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hasil Sort :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(419, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 58);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Metode \r\nSorting :";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(424, 100);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 20);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Quick Sort";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(424, 126);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(90, 20);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Radix Sort";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(424, 153);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(89, 20);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Heap Sort";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(424, 180);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(98, 20);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Bubble Sort";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(424, 206);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 20);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Shell Sort";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(28, 57);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(137, 22);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Process Time :";
+            // 
+            // ProcessTime
+            // 
+            this.ProcessTime.AutoSize = true;
+            this.ProcessTime.Location = new System.Drawing.Point(421, 354);
+            this.ProcessTime.Name = "ProcessTime";
+            this.ProcessTime.Size = new System.Drawing.Size(43, 16);
+            this.ProcessTime.TabIndex = 14;
+            this.ProcessTime.Text = "*Time";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(424, 501);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 38);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Process";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ProcessTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxHasil);
+            this.Controls.Add(this.listBoxRaw);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Data Structure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxRaw;
+        private System.Windows.Forms.ListBox listBoxHasil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ProcessTime;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Project_UAS_DataStructure/Project_UAS_DataStructure/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_UAS_DataStructure
+{
+    public partial class Form1 : Form
+    {
+        private int[] dataRaw;
+        private int[] dataProceed;
+        private string type = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Quick Sort
+            type = "Quick";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //Radix Sort
+            type = "Radix";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            //Heap Sort
+            type = "Heap";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            //Bubble Sort
+            type = "Bubble";
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            //Shell Sort
+            type = "Shell";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Generate Random
+            listBoxRaw.Items.Clear();
+            int counter = (int)numericUpDown1.Value;
+            dataRaw = new int[counter];
+            dataProceed = new int[counter];
+
+            Random objRandom = new Random();
+            for (int i = 0; i < counter; i++)
+            {
+                int tampungAngka = objRandom.Next(0, 10000);
+                dataRaw[i] = tampungAngka;
+            }
+
+            for (int i = 0; i < dataRaw.Length; i++)
+            {
+                listBoxRaw.Items.Add(dataRaw[i]);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBoxHasil.Items.Clear();
+            //Process Sort
+            if (type == "Quick")
+            {
+
+            }
+            else if (type == "Radix")
+            {
+
+            }
+            else if (type == "Heap")
+            {
+
+            }
+            else if (type == "Bubble")
+            {
+
+            }
+            else if (type == "Shell")
+            {
+
+            }
+        }
+
+        private int[] QuickSort()
+        {
+            
+            return dataProceed;
+        }
+
+        private int[] RadixSort()
+        {
+
+            return dataProceed;
+        }
+
+        private int[] HeapSort()
+        {
+
+            return dataProceed;
+        }
+
+        private int[] BubbleSort()
+        {
+
+            return dataProceed;
+        }
+
+        private int[] ShellSort()
+        {
+
+            return dataProceed;
+        }
+
+    }
+}
