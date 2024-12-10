8acfe84769e56df74554926aee6de8cab50c124e
ArthurPts <hans.2020185@smapetrus.net>
2024-12-10 15:19:13 +0700

    radix setengah jalan

--- Project_UAS_DataStructure/Project_UAS_DataStructure/Form1.cs
+++ Project_UAS_DataStructure/Project_UAS_DataStructure/Form1.cs
@@ -15,7 +15,7 @@ namespace Project_UAS_DataStructure
     {
         private int[] data;
         private string type = "";
-        private int counter;
+        private int counter; // untuk menentukan panjang arr
 
         public Form1()
         {
@@ -101,44 +101,28 @@ namespace Project_UAS_DataStructure
             }
             else if (type == "Radix")
             {
-                List<string> arr0 = new List<string>();
-                List<string> arr1 = new List<string>();
-                List<string> arr2 = new List<string>();
-                List<string> arr3 = new List<string>();
-                List<string> arr4 = new List<string>();
-                List<string> arr5 = new List<string>();
-                List<string> arr6 = new List<string>();
-                List<string> arr7 = new List<string>();
-                List<string> arr8 = new List<string>();
-                List<string> arr9 = new List<string>();
-                int iterasi = data.
+                int iterasi = data.Max().ToString().Length;
+                string[] tampung = new string[counter];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    tampung[i] = data[i].ToString().PadLeft(iterasi,'0');
+                }
+
+                RadixSort(tampung, iterasi);
+                
 
 
                 var waktu = new Stopwatch();
                 waktu.Start();
 
 
-                int[] coba = new int[5];
-                coba[0] = 2;
-                coba[1] = 3;
-                coba[2] = 4;
-                coba[3] = 50120;
-                coba[4] = 6;
-                string te = "009";
-
-                int counter = coba.Max();
-                int iterasi = counter.ToString().Length;
-                MessageBox.Show(int.Parse(te).ToString());
-
-                RadixSort(coba);
-
                 waktu.Stop();
                 ProcessTime.Text = $"{waktu.ElapsedMilliseconds} ms";
 
-                //for (int i = 0; i < data.Length; i++)
-                //{
-                //    listBoxHasil.Items.Add(data[i]);
-                //}
+                for (int i = 0; i < data.Length; i++)
+                {
+                    listBoxHasil.Items.Add(data[i]);
+                }
             }
             else if (type == "Heap")
             {
@@ -192,23 +176,86 @@ namespace Project_UAS_DataStructure
             return daftarData;
         }
 
-        private int[] RadixSort(int[] daftarData)
+        private int[] RadixSort(string[] daftarData, int iterasi)
         {
-            
-
-           for (int i = 0;i < daftarData.Length; i++) //iterasi sesuai panjang angka
+            for (int i = 0; i < iterasi; i++) //iterasi sesuai panjang angka
             {
+                int indexAmbil = iterasi - (i+1);
 
-            }
-            for (int j = 0; j < daftarData.Length; j++) //untuk masukin data ke index baru
-            {
-                if (daftarData[i])
+                List<string>[] daftarTampung = new List<string>[10];
+
+                for (int j = 0; i < daftarTampung.Length; i++) // Initialize each list
                 {
+                    daftarTampung[i] = new List<string>();  
+                }
+
+                for (int j = 0; j < daftarTampung.Length; j++) //untuk masukin data ke index baru
+                {
+                    if (daftarData[j][indexAmbil] == '0')
+                    {
+                        daftarTampung[0].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '1')
+                    {
+                        daftarTampung[1].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '2')
+                    {
+                        daftarTampung[2].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '3')
+                    {
+                        daftarTampung[3].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '4')
+                    {
+                        daftarTampung[4].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '5')
+                    {
+                        daftarTampung[5].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '6')
+                    {
+                        daftarTampung[6].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '7')
+                    {
+                        daftarTampung[7].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '8')
+                    {
+                        daftarTampung[8].Add(daftarData[j]);
+                    }
+
+                    if (daftarData[j][indexAmbil] == '9')
+                    {
+                        daftarTampung[9].Add(daftarData[j]);
+                    }
+
+
 
                 }
+
+                List<string> listFinal = new List<string>();
+
+                foreach (var list in daftarTampung)
+                {
+                    listFinal.AddRange(list);
+                }
+
+
             }
 
-            
+
             return daftarData;
         }
 
@@ -276,6 +323,7 @@ namespace Project_UAS_DataStructure
             listBoxHasil.Items.Clear();
             listBoxRaw.Items.Clear();
             ProcessTime.Text = "-";
+
         }
     }
 }
