d35359e08e9ed2ac73c3c8b4216e7119d5a96f67
EylinJois <s160423031@student.ubaya.ac.id>
2025-01-16 21:02:56 +0700

    shell sort hilangin commentny spy g kyk copas

--- Project_UAS_DataStructure/Project_UAS_DataStructure/Form1.cs
+++ Project_UAS_DataStructure/Project_UAS_DataStructure/Form1.cs
@@ -415,23 +415,13 @@ namespace Project_UAS_DataStructure
         {
             int n = daftarData.Length;
 
-            // Start with a big gap, 
-            // then reduce the gap
+            // Start with a big gap, then reduce the gap
             for (int gap = n / 2; gap > 0; gap /= 2)
             {
-                // Do a gapped insertion sort for this gap size.
-                // The first gap elements a[0..gap-1] are already
-                // in gapped order keep adding one more element
-                // until the entire array is gap sorted
                 for (int i = gap; i < n; i += 1)
                 {
-                    // add a[i] to the elements that have
-                    // been gap sorted save a[i] in temp and
-                    // make a hole at position i
                     int temp = daftarData[i];
 
-                    // shift earlier gap-sorted elements up until
-                    // the correct location for a[i] is found
                     int j;
 
                     if (AscendingBtn.Checked)
@@ -448,8 +438,6 @@ namespace Project_UAS_DataStructure
                             daftarData[j] = daftarData[j - gap];
                         }
                     }
-                    // put temp (the original a[i]) 
-                    // in its correct location
                     daftarData[j] = temp;
                 }
             }
